650eb997202cc3e13af7a9ffd5d80668003fb633
AmenoAmena <ardacavusoglu666@gmail.com>
2024-05-20 21:35:38 +0300

    A selenium automation for my website

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 import csv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -13,7 +14,7 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),optio
 driver.get("https://www.xn--trkeiir-wxa0q61b.com/")
 
 links = driver.find_elements(By.CLASS_NAME,"poetLink")
-all_windows = driver.window_handles
+
 
 done_titles = []
 
@@ -21,27 +22,39 @@ poem_dict = {
 
 }
 
+
 for link in links:
-    inner_link = link.get_attribute("innerHTML")
-    current_window = driver.current_window_handle
-    if inner_link not in done_titles:
-        link.click()
-        for windows in all_windows:
-            if windows != current_window:
-                driver.switch_to.window(windows)
-                break
-        poet = driver.find_element(By.TAG_NAME,"h1")
-        poet_name = poet.find_element(By.TAG_NAME,"a")
-        poem = driver.find_element(By.TAG_NAME,"pre")
+    try:
+        current_window = driver.current_window_handle
+        inner_link = link.get_attribute("innerHTML")
+        sleep(2)
+        if inner_link not in done_titles:
+            link.click()
 
-        poet_name_real = poet_name.get_attribute("innerHTML") 
-        poem_name = poem.get_attribute("innerHTML")
+            all_windows = driver.window_handles
+            for windows in all_windows:
+                if windows != current_window:
+                    driver.switch_to.window(windows)
+                    break
+            
+            poet = driver.find_element(By.TAG_NAME,"h1")
+            poet_name = poet.find_element(By.TAG_NAME,"a")
+            poem = driver.find_element(By.TAG_NAME,"pre")
 
-        poem_dict.update({f"{poet_name_real}":f"{poem_name}"})
-        
-        done_titles.append(poet_name_real)
+            poet_name_real = poet_name.get_attribute("innerHTML") 
+            poem_name = poem.get_attribute("innerHTML")
 
-        home_link = driver.find_element(By.TAG_NAME,"a")
+            poem_dict.update({f"{poet_name_real}":f"{poem_name}"})
+
+            sleep(2)
+
+            driver.back()
+            print(done_titles)
+    except:
+        print("not worked")
+        continue
+    
+
                 
 with open("poem.csv","w",newline="") as poem_file:
     writer = csv.writer(poem_file, delimiter=",")
