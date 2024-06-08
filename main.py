197b7ca54c9f0c065064090779da593e5c517d5d
AmenoAmena <ardacavusoglu666@gmail.com>
2024-06-08 12:30:24 +0300

    bir şeyler değişecek

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 
 options = Options()
@@ -13,7 +15,10 @@ options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 driver.get("https://www.xn--trkeiir-wxa0q61b.com/")
 
-links = driver.find_elements(By.CLASS_NAME,"poetLink")
+links = WebDriverWait(driver,10).until(
+    expected_conditions.presence_of_all_elements_located((By.CLASS_NAME,"poetLink"))
+)
+
 
 
 done_titles = []
@@ -27,8 +32,9 @@ for link in links:
     try:
         current_window = driver.current_window_handle
         inner_link = link.get_attribute("innerHTML")
-        sleep(2)
+        sleep(4)
         if inner_link not in done_titles:
+            print(inner_link)
             link.click()
 
             all_windows = driver.window_handles
@@ -37,14 +43,31 @@ for link in links:
                     driver.switch_to.window(windows)
                     break
             
-            poet = driver.find_element(By.TAG_NAME,"h1")
+#links = WebDriverWait(driver,10).until(
+#    expected_conditions.presence_of_all_elements_located((By.CLASS_NAME,"poetLink"))
+#)
+
+
+            poet = WebDriverWait(driver,10).until(
+                expected_conditions.presence_of_element_located((By.TAG_NAME,"h1"))
+            )
+
+            poet_name = WebDriverWait(driver,10).until(
+                expected_conditions.presence_of_element_located((By.TAG_NAME,"a"))
+            )
+
+            poem = WebDriverWait(driver,10).until(
+                expected_conditions.presence_of_element_located((By.CLASS_NAME,"poetShow"))
+            )
+
+
             poet_name = poet.find_element(By.TAG_NAME,"a")
-            poem = driver.find_element(By.TAG_NAME,"pre")
+            poem = driver.find_element(By.CLASS_NAME,"poetShow")
 
             poet_name_real = poet_name.get_attribute("innerHTML") 
-            poem_name = poem.get_attribute("innerHTML")
+            poem_name = poem.find_element(By.TAG_NAME,"h1").get_attribute("innerHTML")
 
-            poem_dict.update({f"{poet_name_real}":f"{poem_name}"})
+            poem_dict.update({f"{poem_name}":f"{poet_name_real}"})
 
             sleep(2)
 
@@ -54,6 +77,7 @@ for link in links:
         print("not worked")
         continue
     
+sleep(2)
 
                 
 with open("poem.csv","w",newline="") as poem_file:
